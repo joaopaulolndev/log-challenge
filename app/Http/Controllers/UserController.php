86b5502579002398223163d1ff420a3d66bf72bf
João Paulo Leite Nascimento <joaopaulo@MacBook-Pro-de-Joao.local>
2021-09-28 19:00:08 +0100

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Resources\User as UserResource;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return AnonymousResourceCollection
+     */
+    public function index()
+    {
+        $users = User::paginate(15);
+        return UserResource::collection($users);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return UserResource
+     */
+    public function store(Request $request)
+    {
+        $user = new User();
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->birthday = $request->input('birthday');
+        $user->gender = $request->input('gender');
+
+        if( $user->save() ){
+            return new UserResource( $user );
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return UserResource
+     */
+    public function show($id)
+    {
+        $user = User::findOrFail( $id );
+        return new UserResource( $user );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return UserResource
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail( $id );
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->birthday = $request->input('birthday');
+        $user->gender = $request->input('gender');
+
+        if( $user->save() ){
+            return new UserResource( $user );
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return UserResource
+     */
+    public function destroy($id)
+    {
+        $user = User::findOrFail( $id );
+        if( $user->delete() ){
+            return new UserResource( $user );
+        }
+    }
+}
